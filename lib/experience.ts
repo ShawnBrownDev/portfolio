770f0c65cc49b2f2482b7b4d1d12a4f525ae8d3a
Shawn Brown <shawnbrowndevelopment@gmail.com>
2025-06-25 19:44:17 -0400

    Update experience.ts

--- lib/experience.ts
+++ lib/experience.ts
@@ -10,25 +10,43 @@ export const experiences: ExperienceItem[] = [
   {
     id: 'exp-1',
     title: 'Started Self Learning',
-    company: '',
+    company: 'Started to learn programming',
     period: '2019',
   },
-  {
+   {
     id: 'exp-2',
-    title: 'Self Employed Full Stack Developer',
-    company: 'Globex',
-    period: '2020-2025',
+    title: 'Started to Learn Lua for Fivem (GTA V Mod)',
+    company: 'Started to learn Lua for Fivem',
+    period: '2019-2020',
   },
-  // {
-  //   id: 'exp-3',
-  //   title: 'Software Engineer',
-  //   company: 'Acmo Corp',
-  //   period: '2015-2020',
-  // },
-  // {
-  //   id: 'exp-4',
-  //   title: 'M.S. in Computer Science',
-  //   company: '',
-  //   period: '',
-  // },
+  {
+    id: 'exp-3',
+    title: 'Started My Journey in React.js  ',
+    company: 'Started to learn React.js',
+    period: '2020-2021',
+  }, 
+   {
+    id: 'exp-4',
+    title: 'Started Learning Tailwind CSS',
+    company: 'Started to learn Tailwind CSS',
+    period: '2021-ongoing',
+  }, 
+  {
+    id: 'exp-5',
+    title: 'Started Learning TypeScript',
+    company: 'Started to learn TypeScript',
+    period: '2021-ongoing',
+  },
+   {
+    id: 'exp-6',
+    title: 'Started Learning Next.js',
+    company: 'Started to learn Next.js',
+    period: '2020-ongoing',
+  },
+  {
+    id: 'exp-7',
+    title: 'Start my journey in Freelancing',
+    company: 'Freelancing',
+    period: '2025-ongoing',
+  },
 ];
